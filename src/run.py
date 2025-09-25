888be0acecf7b72c5605b6d32fadb1f68a409cdd
chniteesh71 <chniteesh71@gmail.com>
2025-09-25 10:31:44 +0530

    updated the workflow

--- src/run.py
+++ src/run.py
@@ -8,8 +8,9 @@ if len(sys.argv) < 2:
 command = sys.argv[1]
 
 if command == "lint":
-    subprocess.run([sys.executable, "-m", "flake8", "src"])
-    subprocess.run([sys.executable, "-m", "black", "--check", "src"])
+    subprocess.run([sys.executable, "-m", "flake8", ".", "--exclude=./src/.venv,./src/__pycache__,./src/.git"])
+    subprocess.run([sys.executable, "-m", "black", "--check", ".", "--exclude", "./src/.venv"])
+
 elif command == "test":
     subprocess.run([sys.executable, "-m", "pytest", "-v"])
 else:
