b9f6a412b65f218211ca1b767d5eb9eef0d11175
chniteesh71 <chniteesh71@gmail.com>
2025-09-25 10:28:24 +0530

    updated the workflow

--- src/run.py
+++ src/run.py
@@ -8,9 +8,8 @@ if len(sys.argv) < 2:
 command = sys.argv[1]
 
 if command == "lint":
-    # Windows-safe
-    subprocess.run([sys.executable, "-m", "flake8", "."])
-    subprocess.run([sys.executable, "-m", "black", "--check", "."])
+    subprocess.run([sys.executable, "-m", "flake8", "src"])
+    subprocess.run([sys.executable, "-m", "black", "--check", "src"])
 elif command == "test":
     subprocess.run([sys.executable, "-m", "pytest", "-v"])
 else:
